4b155c51a36aef5665735f37f8d9903f0563c665
Eungjlee <eungjlee@smilegate.com>
2021-05-06 16:56:19 +0900

    feat: study 5일차 (기본 폼 작성)

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app">
+    <app-header v-bind:propsdata="str" v-on:renew="renewString" />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+export default {
+  data() {
+    return {
+      str: "Header",
+    };
+  },
+  components: {
+    AppHeader,
+  },
+  methods: {
+    renewString: function() {
+      this.str = "hi";
+    },
+  },
+};
+</script>
+
+<style></style>
---
+++ vue-form/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <form v-on:submit.prevent="submitForm">
+      <div>
+        <label for="username">id : </label>
+        <input type="text" id="username" v-model="username" />
+      </div>
+      <div>
+        <label for="password">pw : </label>
+        <input type="password" id="password" v-model="password" />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.username + " " + this.password);
+      var url = "https://jsonplaceholder.typicode.com/users";
+      var data = {
+        username: this.username,
+        password: this.password,
+      };
+      axios
+        .post(url, data)
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+  },
+};
+</script>
+
+<style></style>
